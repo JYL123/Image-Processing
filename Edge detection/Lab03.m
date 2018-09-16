ad1596c25bfa85390d789f63ce0c9e547145e1c5
Li JiaYao <li474412210@gmail.com>
2018-09-16 22:28:38 +0800

    Add files via upload

--- Edge detection/Lab03.m
+++ Edge detection/Lab03.m
@@ -1,43 +1,85 @@
 close all;
 clear all;
 
-% experiment on one picture first
-FNames = {'meteora_gray.jpg';};
-%'penang_hill_gray.jpg'; 'foggy_carpark_gray.jpg'
-
-%picture meteora_gray.jpg: upper cutoff 235, lower cut off 15, scale:
-%(255-0)/(235-15) = 1.159 (51/44)
-
-
-
 % Task 1: contrast stretch
-% for p = 1 : size(FNames)
-%     figH = figure;
-%     baseName = FNames{p}(1:find(FNames{p}=='.')-1); 
-%     old_figName = strcat(baseName, '.jpg');
-%     
-%     % histogram equalization
-%     A = imread(old_figName, 'jpg');
-%     contrastStretch(A, 1.159);
-%     
-%     figName = strcat(baseName, '_histogram_eq_results.jpg');
-%     print(figH, '-djpeg', figName); 
-% end
+FNames = {'meteora_gray.jpg'; 'penang_hill_gray.jpg'; 'foggy_carpark_gray.jpg'};
+
+%for the entire picture 
+%picture meteora_gray.jpg: upper cut off 235, lower cut off 15
+%scale: (255-0)/(235-20) = 1.186
+
+%picture penang_hill_gray.jpg: upper cut off 200, lower cut off 0
+%scale: (255-0)/(200-0) = 1.275
+
+%picture foggy_carpark_gray.jpg: upper cut off 210, lower cut off 45
+%scale: (255-0)/(240-30) = 1.214
+for p = 1 : size(FNames)
+     figH = figure;
+     baseName = FNames{p}(1:find(FNames{p}=='.')-1); 
+     old_figName = strcat(baseName, '.jpg');
+     
+     A = imread(old_figName, 'jpg');
+     if (strcmp(old_figName,'meteora_gray.jpg'))
+      contrastStretch(A, 1.186);
+     elseif(strcmp(old_figName,'penang_hill_gray.jpg'))
+      contrastStretch(A, 1.275);
+     elseif(strcmp(old_figName,'foggy_carpark_gray.jpg'))
+      contrastStretch(A, 1.275); 
+     end
+     
+     figName = strcat(baseName, '_contrast_result.jpg');
+     print(figH, '-djpeg', figName); 
+end
+
+%for halved picture 
+%picture meteora_gray.jpg: upper cut off 235, lower cut off 15
+%scale: (255-0)/(210-5) = 1.244
+
+%picture penang_hill_gray.jpg: upper cut off 200, lower cut off 0
+%scale: (255-0)/(200-10) = 1.417
+
+%picture foggy_carpark_gray.jpg: upper cut off 210, lower cut off 45
+%scale: (255-0)/(160-30) = 1.962
+for p = 1 : size(FNames)
+     figH = figure;
+     baseName = FNames{p}(1:find(FNames{p}=='.')-1); 
+     old_figName = strcat(baseName, '.jpg');
+     
+     A = imread(old_figName, 'jpg');
+     croppedPicture = A(floor(size(A,1)/2):size(A,1), 1:size(A,2));
+     if (strcmp(old_figName,'meteora_gray.jpg'))
+      contrastStretch(croppedPicture, 1.244);
+     elseif(strcmp(old_figName,'penang_hill_gray.jpg'))
+      contrastStretch(croppedPicture, 1.417);
+     elseif(strcmp(old_figName,'foggy_carpark_gray.jpg'))
+      contrastStretch(croppedPicture, 1.759); 
+     end
+     
+     figName = strcat(baseName, '_half_pic_contrast_result.jpg');
+     print(figH, '-djpeg', figName); 
+end
 
 % Task 2: 1D convolution
-% h = [-1 0 1 0 0 0 0 0 0 0];
-% x = [5 5 5 5 5 0 0 0 0 0];
-% OneDConvolution(h,x);
+h = [-1 0 1 0 0 0 0 0 0 0];
+x = [5 5 5 5 5 0 0 0 0 0];
+%OneDConvolution(h,x);
 
 %Task 3: Edge detection
-RGB = imread('carmanBox.jpg');
-I = rgb2gray(RGB);
-% figure
-% imshow(I)
-EdgeDetection(I);
+FNames = {'checker.jpg'; 'letterBox.jpg'; 'pipe.jpg'; 'carmanBox.jpg';};
+for p = 1 : size(FNames)
+    figH = figure;
+    baseName = FNames{p}(1:find(FNames{p}=='.')-1); 
+    old_figName = strcat(baseName, '.jpg');
+        
+    RGB = imread(old_figName, 'jpg');
+    I = rgb2gray(RGB);
+    EdgeDetection(I);
+    
+    figName = strcat(baseName, '_sobel_edge_detection.jpg');
+    print(figH, '-djpeg', figName); 
+end
 
 function contrastStretch(A, x)
-    x
     subplot(3,2,1), imshow(A, [0 255]);
     title('original image');
 
@@ -128,7 +170,7 @@ function OneDConvolution(h, x)
         csum = sum(paddedX .* temp);
         conv_result(m+10) = csum;
 
-        subplot(4,1,4), stem([-9:9], conv_result, 'k'), ylim([0 50]), title('convolution result');
+        subplot(4,1,4), stem([-9:9], conv_result, 'k'), ylim([-7 7]), title('convolution result');
 
         pause(5)
 
@@ -140,5 +182,39 @@ function OneDConvolution(h, x)
 end
 
 function EdgeDetection(I)
+    %Padding
+    paddedImg=zeros(size(I, 1)+4, size(I, 2)+4);
+    for r = 4 : size(paddedImg, 1)-4
+        for c = 4 : size(paddedImg, 2)-4
+            intensity = I(r-1,c-1);
+            paddedImg(r,c)=intensity; 
+        end
+    end 
+    paddedImg
+    edgeDetect = detect(paddedImg);
+    imshow(edgeDetect);
+   
     
 end
+
+function edgeDetect = detect(img)
+    %kernels will be used:
+    %Kx = [-1 0 1; -2 0 2; -1 0 1];
+    %Ky = [-1 -2 -1; 0 0 0; 1 2 1];
+    
+    % use double in case of overflow
+    I = double(img);
+    
+     for r = 2 : size(img, 1)-1
+         for c = 2 : size(img, 2)-1
+             
+             %multiplication & addition
+             sumX = I(r-1, c-1)*(-1) + I(r-1, c+1) + I(r, c-1)*(-2) + I(r, c+1)*(2) + I(r+1, c-1)*(-1) + I(r+1, c+1)*(1);
+             sumY = I(r-1, c-1)*(-1) + I(r-1, c)*(-2) + I(r-1, c+1)*(-1) + I(r+1, c-1)*(1) +  I(r+1, c)*(2) + I(r+1, c+1)*(1);
+            
+             %reset value
+             img(r, c) = sqrt(sumX^2 + sumY^2);
+         end
+     end
+    edgeDetect = img;
+end 
