9fb0b3fb938f88cdf3adc89f7b117292f582a6fe
Li JiaYao <li474412210@gmail.com>
2018-10-17 17:22:28 +0800

    Add files via upload

--- Projection-Quanternion/Lab06.m
+++ Projection-Quanternion/Lab06.m
@@ -11,6 +11,7 @@ pts(6,: ) = [1 -1 -1];
 pts(7,: ) = [1 -1 -1];
 pts(8,: ) = [1 -1 -1];
 
+% represent camera position in a world coordination
 initialPosition = [0 0 -5]; % add Sp = 0
 theta = pi/6; % rotation angle
 n = [0; 1; 0]; % rotation axis
@@ -21,27 +22,92 @@ cam_pos(1, : ) = [ 0 0 -5];
 %first rotation 
 %R1= rotate(r, n, theta);
 %cam_pos(2, : ) = [R1(2,1) R1(3,1) R1(4,1)];
+R1 = rotateByEquation(r, n, theta);
+cam_pos(2, : ) = R1.';
 
 %second rotation
 %R1 = R1.';
 %R2= rotate(R1, n, theta);
 %cam_pos(3, : ) = [R2(2,1) R2(3,1) R2(4,1)];
+R1 = [0 R1(1,1) R1(2,1) R1(3,1)];
+R2 = rotateByEquation(R1, n, theta);
+cam_pos(3, : ) = R2.';
 
 %third rotation
 %R2 = R2.';
 %R3= rotate(R2, n, theta);
 %cam_pos(4, : ) = [R3(2,1) R3(3,1) R3(4,1)];
+R2 = [0 R2(1,1) R2(2,1) R2(3,1)];
+R3 = rotateByEquation(R2, n, theta);
+cam_pos(4, : ) = R3.';
+cam_pos
 
-%cam_pos;
+% use roll, pitch and yaw to represent rotation
+% represent world coordination in a camera perspective?
+% w = 0; phi = 30 degree (pi/6) (for the first rotation); k = 0;
+w = 0;
+phi = pi/6;
+k = 0;
+rpymat_1 = [1 0 0; 0 1 0; 0 0 1];
+rpymat_2 = rpyRotation(rpymat_1, w, pi/6, k) % 30 degree
+rpymat_3 = rpyRotation(rpymat_2, w, pi/3, k) % 60 degree
+rpymat_4 = rpyRotation(rpymat_3, w, pi/2, k) % 90 degree
+
+% rotation represented by quanternion
+% rotational axis = [ 0; 1; 0] (3x1 row matrix)
+% rotation degree = 30
+% define q as colnum matrix
+w = [0; 1; 0];
+quatmat_2 = quanRotation(pi/6, w)
+quatmat_3 = quanRotation(pi/3, w)
+quatmat_4 = quanRotation(pi/2, w)
+
+%Projecting the 3D shapes
+nframes = 4;
+npts = size(pts,1);
+U = zeros(nframes, npts); V = zeros(nframes, npts);
+
+for fr = 1 : nframes
+subplot(2,2,fr), plot(U(fr,:), V(fr,:), '*'); for p = 1 : npts
+text(U(fr,p)+0.02, V(fr,p)+0.02, num2str(p)); end
+end
 
 
- % checking
- v_p = [r(1,2) r(1,3) r(1,4)] % 0,0,-5
- n
- cross(n, v_p.')
- c = (v_p*cos(theta)).' + dot(v_p, n)*n*(1-cos(theta)) + (cross(n, v_p.'))*sin(theta)    
 
-function R = rotate (r, n, theta)
+
+
+
+
+% quanternion rotation 
+% 1x4 column matrix as quanternion, rotational degree as theta, rotational
+% axis w
+function Rt = quanRotation(theta, w)
+q = [cos(theta/2) sin(theta/2)*w(1,1) sin(theta/2)*w(2,1) sin(theta/2)*w(3,1)];
+    Rt = [
+        q(1,1)^2+q(1,2)^2-q(1,3)^2-q(1,4)^2 2*(q(1,2)*q(1,3)-q(1,1)*q(1,4)) 2*(q(1,2)*q(1,4)+q(1,1)*q(1,3));
+        2*(q(1,2)*q(1,3) + q(1,1)*q(1,4)) q(1,1)^2+q(1,3)^2-q(1,2)^2-q(1,4)^2 2*(q(1,3)*q(1,4)-q(1,1)*q(1,2));
+        2*(q(1,2)*q(1,4)-q(1,1)*q(1,3)) 2*(q(1,3)*q(1,4)+q(1,1)*q(1,2)) q(1,1)^2+q(1,4)^2-q(1,2)^2-q(1,3)^2;
+    ];
+end
+
+%rpy rotation with phi = pi/6
+function Rt = rpyRotation(R0, w, phi, k)
+    Rt = [
+    cos(k)*cos(phi) cos(k)*sin(phi)-sin(k)*cos(w) cos(k)*sin(phi)*cos(w)+sin(k)*sin(w);     
+    sin(k)*cos(phi) sin(k)*sin(phi)*sin(w)+cos(k)*cos(w) sin(k)*sin(phi)*cos(w)-cos(k)*sin(w);
+    -sin(phi) cos(phi)*sin(w) cos(phi)*cos(w);
+    ];
+end 
+
+% checking
+% r (4x1): initial position; n: rotation axis; theta: rotation angle
+function R = rotateByEquation(r, n, theta)
+    v_p = [r(1,2) r(1,3) r(1,4)]; % 0,0,-5
+    c = (v_p*cos(theta)).' + dot(v_p, n)*n*(1-cos(theta)) + (cross(n, v_p.'))*sin(theta);
+    R = c;
+ end 
+ 
+function R = rotateByMatrix (r, n, theta)
     q = [cos(theta/2) sin(theta/2)*r(1,2) sin(theta/2)*r(1,3) sin(theta/2)*r(1,4)];
     Q = [cos(theta/2) -sin(theta/2)*r(1,2) -sin(theta/2)*r(1,3) -sin(theta/2)*r(1,4)];
     p = [0 r(1,2) r(1,3) r(1, 4)];
