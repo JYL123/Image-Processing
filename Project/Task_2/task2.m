f03006ac8f0aace8440607b37477dcd362129112
Li JiaYao <li474412210@gmail.com>
2018-11-14 16:09:03 +0800

    Add files via upload

--- Project/Task_2/task2.m
+++ Project/Task_2/task2.m
@@ -10,6 +10,7 @@ world_cooridnate = [
 
 %scene_point = [ x; y; z; 1]
 
+% T,R to be corrected line 73
 % camera 1 
 R1 = [
     0.96428667991264605 -0.26484969138677328 -0.0024165916859785336;
@@ -23,7 +24,6 @@ T1 = [
     2.2444637695699150;
     ];
 
-cam_pos_1 = R1* world_cooridnate + T1;
 
 caliberation_1 = [
     870.14531487461625 0 949.42001822880479 0;
@@ -44,8 +44,6 @@ T2 = [
     2.2750378317324982;
     ];
 
-cam_pos_2 = R2* world_cooridnate + T2;
-
 caliberation_2 = [
     893.34367240024267 0 949.96816131377727 0;
     0 893.34367240024267 546.79562177577259 0;
@@ -65,7 +63,6 @@ T3 = [
     2.2979640654841407; 
     ];
 
-cam_pos_3 = R3* world_cooridnate + T3;
 
 caliberation_3 = [
     872.90852997159800 0 944.45161471037636 0;
@@ -83,117 +80,113 @@ R_1 = R1;
 R_2 = R2;
 R_3 = R3;
 
-% test point from frame 3
+% test point from frame 10
 % 1920 1080, convert picture point to camera image point
-% c1: 749.430935, 248.147808
-% c2: 698.955519, 254.998536
-% c_1 = [544.195114-1080/2; 297.948346-1920/2; 1];
-% c_2 = [567.523424-1080/2; 287.74321-1920/2; 1];
+% camera 1 data
 c_1 = [749.430935-1920/2; 248.147808-1080/2; 1];
-c_2 = [698.955519-1920/2; 254.998536-1080/2; 1];
+c_11 = [732.621677-1920/2; 255.100929-1080/2; 1];
+c_12 = [713.485208-1920/2; 259.792953-1080/2; 1];
+c_13 = [709.707415/2-1920/2; 274.646952-1080/2; 1];
+c_14 = [669.426554/2-1920/2; 273.152385-1080/2; 1];
 
+% camera 2 data
+c_2 = [698.955519-1920/2; 254.998536-1080/2; 1];
+c_21 = [687.08528-1920/2; 258.854223-1080/2; 1];
+c_22 = [672.884517-1920/2; 261.464359-1080/2; 1];
+c_23 = [647.68956-1920/2; 269.011233-1080/2; 1];
+c_24 = [636.423067-1920/2; 268.393115-1080/2; 1];
+
+% camera 3 data
+c_3 = [1235.686836-1920/2; 246.513591-1080/2];
+c_31 = [1206.700476-1920/2; 251.386177-1080/2];
+c_32 = [1194.403231-1920/2; 234.134829-1080/2];
+c_33 = [1172.533138-1920/2; 236.305715-1080/2];
+c_34 = [1157.526919-1920/2; 233.625535-1080/2];
 
 % For camera 1
-U_1  = computePerspectiveU(caliberation_1, R_1, c_1, T_1);
-V_1  = computePerspectiveV(caliberation_1, R_1, c_1, T_1);
+U_1  = computePerspectiveWithU(caliberation_1, R_1, c_1, T_1);
+V_1  = computePerspectiveWithV(caliberation_1, R_1, c_1, T_1);
+U_1one  = computePerspectiveWithU(caliberation_1, R_1, c_11, T_1);
+V_1one  = computePerspectiveWithV(caliberation_1, R_1, c_11, T_1);
+U_1two  = computePerspectiveWithU(caliberation_1, R_1, c_12, T_1);
+V_1two  = computePerspectiveWithV(caliberation_1, R_1, c_12, T_1);
+U_1three  = computePerspectiveWithU(caliberation_1, R_1, c_13, T_1);
+V_1three  = computePerspectiveWithV(caliberation_1, R_1, c_13, T_1);
+U_1four  = computePerspectiveWithU(caliberation_1, R_1, c_14, T_1);
+V_1four  = computePerspectiveWithV(caliberation_1, R_1, c_14, T_1);
+
 
 % For camera 2
-U_2  = computePerspectiveU(caliberation_2, R_2, c_2, T_2);
-V_2  = computePerspectiveV(caliberation_2, R_2, c_2, T_2);
+U_2  = computePerspectiveWithU(caliberation_2, R_2, c_2, T_2);
+V_2  = computePerspectiveWithV(caliberation_2, R_2, c_2, T_2);
+U_2one  = computePerspectiveWithU(caliberation_2, R_2, c_21, T_2);
+V_2one  = computePerspectiveWithV(caliberation_2, R_2, c_21, T_2);
+U_2two  = computePerspectiveWithU(caliberation_2, R_2, c_22, T_2);
+V_2two  = computePerspectiveWithV(caliberation_2, R_2, c_22, T_2);
+U_2three  = computePerspectiveWithU(caliberation_2, R_2, c_23, T_2);
+V_2three  = computePerspectiveWithV(caliberation_2, R_2, c_23, T_2);
+U_2four  = computePerspectiveWithU(caliberation_2, R_2, c_24, T_2);
+V_2four  = computePerspectiveWithV(caliberation_2, R_2, c_24, T_2);
+
+% For camera 3
+U_3  = computePerspectiveWithU(caliberation_3, R_3, c_3, T_3);
+V_3  = computePerspectiveWithV(caliberation_3, R_3, c_3, T_3);
+U_3one  = computePerspectiveWithU(caliberation_3, R_3, c_31, T_3);
+V_3one  = computePerspectiveWithV(caliberation_3, R_3, c_31, T_3);
+U_3two  = computePerspectiveWithU(caliberation_3, R_3, c_32, T_3);
+V_3two  = computePerspectiveWithV(caliberation_3, R_3, c_32, T_3);
+U_3three  = computePerspectiveWithU(caliberation_3, R_3, c_33, T_3);
+V_3three  = computePerspectiveWithV(caliberation_3, R_3, c_33, T_3);
+U_3four  = computePerspectiveWithU(caliberation_3, R_3, c_34, T_3);
+V_3four  = computePerspectiveWithV(caliberation_3, R_3, c_34, T_3);
 
 % svd 
-M = zeros(4,4);
+M = zeros(30,4);
 M(1, :) = U_1;
 M(2, :) = V_1;
-M(3, :) = U_2;
-M(4, :) = V_2;
+M(3, :) = U_1one;
+M(4, :) = V_1one;
+M(5, :) = U_1two;
+M(6, :) = V_1two;
+M(7, :) = U_1three;
+M(8, :) = V_1three;
+M(9, :) = U_1four;
+M(10, :) = V_1four;
+
+M(11, :) = U_2;
+M(12, :) = V_2;
+M(13, :) = U_2one;
+M(14, :) = V_2one;
+M(15, :) = U_2two;
+M(16, :) = V_2two;
+M(17, :) = U_2three;
+M(18, :) = V_2three;
+M(19, :) = U_2four;
+M(20, :) = V_2four;
+
+M(21, :) = U_3;
+M(22, :) = V_3;
+M(23, :) = U_3one;
+M(24, :) = V_3one;
+M(25, :) = U_3two;
+M(26, :) = V_3two;
+M(27, :) = U_3three;
+M(28, :) = V_3three;
+M(29, :) = U_3four;
+M(30, :) = V_3four;
 
 [U, S, V] = svd(M)
 
-% caliberation =  intrinsic parameter
-% orientation matrix = rotatio matrix
-% position matrix = translation matrix
-
-% For camera 1
-U_11  = computePerspectiveUTest1(caliberation_1, R_1, c_1, T_1);
-V_11  = computePerspectiveVTest1(caliberation_1, R_1, c_1, T_1);
-
-% For camera 2
-U_21  = computePerspectiveUTest1(caliberation_2, R_2, c_2, T_2);
-V_21  = computePerspectiveVTest1(caliberation_2, R_2, c_2, T_2);
-
-% svd 
- N = zeros(4,4);
- N(1, :) = U_11;
- N(2, :) = V_11;
- N(3, :) = U_21;
- N(4, :) = V_21;
-% 
- [U1, S1, V1] = svd(N)
-% 
-% 
-% figure
-% plot3(R_1(1, :), R_1(2, :), R_1(3, :))
-check = [0.2838;
-         -1.1227;
-         -0.1112;]
-     
-%checkpoint(caliberation_3, R_3, T_3, check)
+%plot3(R_1(:,1)+T_1(1,1), R_1(:,2)+T_1(2,1), R_1(:,3)+T_1(3,1))
+% get [X, Y, Z] from V 
+check = [ V(1,4)/V(4,4);  V(2,4)/V(4,4);  V(3,4)/V(4,4);] 
+%check = [0.3464;  1.0185;  -0.2457;] 
+checking_result = checkpoint(caliberation_2, R_2, T_2, check)
 
 
-function U = computePerspectiveUTest1(caliberation, orientation, twoDpoint, position)
-    a = [caliberation(1,1) caliberation(1,3) -1*twoDpoint(1,1)];
-    b_1 = [orientation(1,1); orientation(3,1); orientation(3,1);];
-    b_2 = [orientation(1,2); orientation(3,2); orientation(3,2);];
-    b_3 = [orientation(1,3); orientation(3,3); orientation(3,3);];
-    b = zeros(3,3);
-    b(:,1) = b_1;
-    b(:,2) = b_2;
-    b(:,3) = b_3;
-    c_11 = [position(1,1); position(3,1); position(3,1);];
-    U = [a*b_1 a*b_2 a*b_3 a*c_11];
-    
-    %method 2:
-%     Extrinsic = [  
-%             orientation(1,1) orientation(1,2) orientation(1,3) position(1,1);
-%             orientation(2,1) orientation(2,2) orientation(2,3) position(2,1);
-%             orientation(3,1) orientation(3,2) orientation(3,3) position(3,1);
-%             0 0 0 1;
-%         ];
-%     X = caliberation*Extrinsic; % resulting 3 by 4 matrix 
-%     U = [twoDpoint(1,1)*X(3,1)-X(1,1) twoDpoint(1,1)*X(3,2)-X(1,2) twoDpoint(1,1)*X(3,3)-X(1,3) twoDpoint(1,1)*X(3,4)-X(1,4)]
-    
-    
-
-end
-
-function V = computePerspectiveVTest1(caliberation, orientation, twoDpoint, position)
-    a = [caliberation(2,2) caliberation(2,3) -1*twoDpoint(2,1)];
-    b_1 = [orientation(2,1); orientation(3,1); orientation(3,1);];
-    b_2 = [orientation(2,2); orientation(3,2); orientation(3,2);];
-    b_3 = [orientation(2,3); orientation(3,3); orientation(3,3);];
-    b = zeros(3,3);
-    b(:,1) = b_1;
-    b(:,2) = b_2;
-    b(:,3) = b_3;
-    c_22 = [position(2,1); position(3,1); position(3,1);];
-    V = [a*b_1 a*b_2 a*b_3 a*c_22];
-    
-    %method 2:
-     Extrinsic = [  
-             orientation(1,1) orientation(1,2) orientation(1,3) position(1,1);
-             orientation(2,1) orientation(2,2) orientation(2,3) position(2,1);
-             orientation(3,1) orientation(3,2) orientation(3,3) position(3,1);
-             0 0 0 1;  
-         ];
-
-%     X = caliberation*Extrinsic; % resulting 3 by 4 matrix 
-%     V = [twoDpoint(2,1)*X(2,1)-X(1,1) twoDpoint(2,1)*X(3,2)-X(2,2) twoDpoint(2,1)*X(3,3)-X(2,3) twoDpoint(2,1)*X(3,4)-X(2,4)]
-
-end
-
-function U = computePerspectiveU(caliberation, orientation, twoDpoint, position)
-    a = [caliberation(1,1) caliberation(1,3) -1*twoDpoint(1,1)];
+% derived from perspectve equation directly
+function Row = computePerspectiveWithU(caliberation, orientation, twoDpoint, position)
+    a = [caliberation(1,1) caliberation(1,3) -1*twoDpoint(1,1)]
     b_1 = [orientation(1,1); orientation(3,1); orientation(3,1);];
     b_2 = [orientation(1,2); orientation(3,2); orientation(3,2);];
     b_3 = [orientation(1,3); orientation(3,3); orientation(3,3);];
@@ -201,13 +194,14 @@ function U = computePerspectiveU(caliberation, orientation, twoDpoint, position)
     b(1,1) = a*b_1;
     b(1,2) = a*b_2;
     b(1,3) = a*b_3;
-    c = position;
+    c = -1*b*position;
     
-    U = [b(:,1) b(:,2) b(:,3) -1*b*c];
+    Row = [b(1,1) b(1,2) b(1,3) c];
 
 end
 
-function V = computePerspectiveV(caliberation, orientation, twoDpoint, position)
+% derived from perspectve equation directly
+function Row = computePerspectiveWithV(caliberation, orientation, twoDpoint, position)
     a = [caliberation(2,2) caliberation(2,3) -1*twoDpoint(2,1)];
     b_1 = [orientation(2,1); orientation(3,1); orientation(3,1);];
     b_2 = [orientation(2,2); orientation(3,2); orientation(3,2);];
@@ -216,24 +210,27 @@ function V = computePerspectiveV(caliberation, orientation, twoDpoint, position)
     b(1,1) = a*b_1;
     b(1,2) = a*b_2;
     b(1,3) = a*b_3;
-    c = position;
+    c = -1*b*position;
     
-    V = [b(:,1) b(:,2) b(:,3) -1*b*c];
+    Row = [b(1,1) b(1,2) b(1,3) c];
 
 end
 
-function checkpoint(caliberation, orientation, twoDpoint, position, check)
+function c = checkpoint(caliberation, orientation, position, check)
     % diff 3*1 matrix
     diff = check-position;
-   
-    u_upper = caliberation(1,3)*diff*orientation(1,:);
-    u_lower = diff*orientation(3,:);
     
-   
+    u_upper = orientation(1,:)*diff;
+    u_lower = orientation(3,:)*diff;
+    u = caliberation(1,1)*(u_upper/u_lower)+caliberation(1,3);
     
-
-u = caliberation(1, 3)*orientation(1,:)
-
+    v_upper = orientation(2,:)*diff;
+    v_lower = orientation(3,:)*diff;
+    v = caliberation(2,2)*(v_upper/v_lower)+caliberation(2,3);
+    
+    c = zeros(2,1);
+    c(1, :) = u;
+    c(2, :) = v;    
 end
 
 
